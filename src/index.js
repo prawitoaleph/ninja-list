4c8145ef06d55b7217f98ad5be540a4f4c4b420a
Prawito Hudoro <prawito@aleph-labs.com>
2019-04-26 15:21:07 +0700

    fixing import funciton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-var uniqueRandomArray = require('unique-random-array');
-var ninjaList = require('./ninja-list.json');
+// var uniqueRandomArray = require('unique-random-array');
+// var ninjaList = require('./ninja-list.json');
 
-module.exports = {
-    all: ninjaList,
-    random: uniqueRandomArray(ninjaList)
+// module.exports = {
+//     all: ninjaList,
+//     random: uniqueRandomArray(ninjaList)
+// }
+
+import uniqueRandomArray from 'unique-random-array';
+import ninjaList from './ninja-list.json';
+
+const all = ninjaList;
+const random = uniqueRandomArray(ninjaList)
+
+export {
+    all,
+    random
 }
